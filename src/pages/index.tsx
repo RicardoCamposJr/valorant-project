7c3effe4077818331c9ea006eb5b4e7b257f8ad0
Ricardo Campos <rickoliver001@hotmail.com>
2024-08-10 10:14:18 -0300

    feat: adding eslint and prettier configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,20 @@
-import type { ReactElement } from 'react'
+import { useEffect, useState, type ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 import Layout from '@/components/Layout'
 import * as S from './styles'
 import { TIndex } from '@/types/TIndex'
- 
-const Home: NextPageWithLayout<TIndex> = ({children}) => {
-  return (
-    <S.Container>
-      {children}
-    </S.Container>
-  )
+
+const Home: NextPageWithLayout<TIndex> = ({ children }) => {
+  const [a, setA] = useState<boolean>(false)
+  useEffect(() => {
+    setA(true)
+  }, [])
+
+  return <S.Container>{children}</S.Container>
 }
- 
+
 Home.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
+  return <Layout>{page}</Layout>
 }
- 
+
 export default Home
